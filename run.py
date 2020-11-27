ea07acda13cd8c913f74b74c82a88454a64c4ea3
NubeDev <aidan@envrioservices.com.au>
2020-11-28 06:55:39 +1100

    sc

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def on_disconnect(mqttc, userdata, rc):
 # MQTT handler
 mqttc = paho.Client()
 mqttc.on_connect = on_connect
-mqttc.username_pw_set(username=broker_username, password=broker_password)
+# mqttc.username_pw_set(username=broker_username, password=broker_password)
 mqttc.connect(broker_url, broker_port, broker_timeout)
 
 
@@ -37,15 +37,18 @@ def on_message_topic2(mosq, obj, msg):
     print("Message payload: ", msg.payload)
     payload = msg.topic
 
-    service = 'modbus_rtu'
+    service = 'cov'
     match = ['rubix', 'points', service]
     t_parts = payload.split("/")
+    print(t_parts, len(t_parts), t_parts[5])
     point_name = None
     point_val = None
     point_fault = None
-    if t_parts[0:3] == match and len(t_parts) == 10:
+
+    if t_parts[0:3] == match and len(t_parts) == 11:
+        print(222222222)
         try:
-            point_name = t_parts[8]
+            point_name = t_parts[5]
             x = json.loads(msg.payload.decode('utf-8'))
             point_val = str("%s" % x['value'])
             point_fault = str("%s" % x['fault'])
@@ -63,7 +66,7 @@ def on_message_topic2(mosq, obj, msg):
     port = 1717
 
     try:
-        if point_name is not None:
+        if not point_val:
             url = f'http://{ip}:{port}/api/bacnet/points/name/{point_name}'
             body = {
                 "priority_array_write": {
@@ -76,14 +79,19 @@ def on_message_topic2(mosq, obj, msg):
             r_p = requests.patch(f'{url}', data=json.dumps(body), headers=headers)
             r_json = r_p.json()
             print(r_json)
+        else:
+            print("ERROR")
 
     except Exception as e:
         print(e)
         pass
 
 
-main_topic = "rubix/points/cov/all/+/+/+/+/+/modbus_rtu"
-mqttc.subscribe("rubix/points/cov/#")
+"rubix/points/cov/all/ec6cf0a5-95b9-47c0-bd2f-927331ef23ff/mode_18_6/1177f4f8-b229-41c8-92b3-3a259a7f2fc9/device_18/fd663f30-06f9-436c-851f-693a5a34011c/mod_network_name hey/modbus_rtu"
+"rubix/points/cov/all/+/mode_18_6/+/device_18/+/+/modbus_rtu"
+# main_topic = "rubix/points/cov/all/#"
+main_topic = "rubix/points/cov/all/+/+/+/+/+/+/modbus_rtu"
+mqttc.subscribe("rubix/points/cov/all/#")
 mqttc.message_callback_add(main_topic, on_message_topic2)
 
 # Run endless loop
